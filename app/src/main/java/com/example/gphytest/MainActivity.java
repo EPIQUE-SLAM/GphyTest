e2e3fec31c7824bcb29658d049bd334d0fe65a51
EPIQUE-SLAM <40473129+EPIQUE-SLAM@users.noreply.github.com>
2020-10-06 19:49:57 +0200

    Results

--- app/src/main/java/com/example/gphytest/MainActivity.java
+++ app/src/main/java/com/example/gphytest/MainActivity.java
@@ -1,12 +1,11 @@
 package com.example.gphytest;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
 
-import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
-import android.content.pm.PackageManager;
+import android.media.MediaPlayer;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -16,6 +15,8 @@ public class MainActivity extends AppCompatActivity {
     private Button btnStart;
     private Button btnExit;
 
+    MediaPlayer media1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +24,9 @@ public class MainActivity extends AppCompatActivity {
 
         btnStart = findViewById(R.id.btnStart);
         btnExit = findViewById(R.id.btnExit);
+
+        Song song = new Song();
+        song.execute();
     }
 
     public void onClickStart(View view){
@@ -34,4 +38,33 @@ public class MainActivity extends AppCompatActivity {
         finish();
     }
 
+    class Song extends AsyncTask<Void, Integer, Void> {
+//        private Context context;
+//        private String restype;
+
+        public void Media() {
+
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) { // Se fait en background du thread UI
+            return null;
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            // Inutile ici, cf doc
+            super.onProgressUpdate(values);
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) { // S'exécute sur le ThreadUI après doInBackground
+            super.onPostExecute(aVoid);
+            // ATTENTION, il faut adapter le code ci-dessous avec vos controles graphiques.
+            //InfoPokemon.this.txtViewType.setText(resname);
+            media1 = MediaPlayer.create(getBaseContext(), R.raw.song);
+            media1.start();
+        }
+
+    }
 }
