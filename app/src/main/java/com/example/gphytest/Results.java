5e5f85f44157b36b8fa44bb0b83a1f02168cd129
EPIQUE-SLAM <40473129+EPIQUE-SLAM@users.noreply.github.com>
2020-10-07 18:33:38 +0200

    Results

--- app/src/main/java/com/example/gphytest/Results.java
+++ app/src/main/java/com/example/gphytest/Results.java
@@ -5,38 +5,22 @@ import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Movie;
 import android.os.Bundle;
 import android.os.Environment;
-import android.os.SystemClock;
-import android.text.format.Time;
 import android.util.Log;
 import android.view.View;
-import android.webkit.WebView;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
-import android.widget.VideoView;
-
-import com.koushikdutta.ion.Ion;
-
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.PrintStream;
-import java.util.Calendar;
 import java.util.Date;
-import java.util.stream.Stream;
-
-import javax.xml.transform.Result;
 
 public class Results extends AppCompatActivity {
 
