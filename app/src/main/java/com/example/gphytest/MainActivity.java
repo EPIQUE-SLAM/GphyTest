108aeaf119758c6b7841f9470e3f4e7a2e38ab75
EPIQUE-SLAM <40473129+EPIQUE-SLAM@users.noreply.github.com>
2020-10-07 09:57:53 +0200

    Results

--- app/src/main/java/com/example/gphytest/MainActivity.java
+++ app/src/main/java/com/example/gphytest/MainActivity.java
@@ -2,16 +2,13 @@ package com.example.gphytest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-/*
-LOL on test ça
- */
+
 public class MainActivity extends AppCompatActivity {
 
     private Button btnStart;
@@ -41,11 +38,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     class Song extends AsyncTask<Void, Integer, Void> {
-//        private Context context;
-//        private String restype;
 
         public void Media() {
-
         }
 
         @Override
@@ -55,15 +49,12 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         protected void onProgressUpdate(Integer... values) {
-            // Inutile ici, cf doc
             super.onProgressUpdate(values);
         }
 
         @Override
         protected void onPostExecute(Void aVoid) { // S'exécute sur le ThreadUI après doInBackground
             super.onPostExecute(aVoid);
-            // ATTENTION, il faut adapter le code ci-dessous avec vos controles graphiques.
-            //InfoPokemon.this.txtViewType.setText(resname);
             media1 = MediaPlayer.create(getBaseContext(), R.raw.song);
             media1.start();
         }
