792c83df076477683adacd7c210262789fdb4700
EPIQUE-SLAM <40473129+EPIQUE-SLAM@users.noreply.github.com>
2020-10-13 09:43:02 +0200

    Results

--- app/src/main/java/com/example/gphytest/MainActivity.java
+++ app/src/main/java/com/example/gphytest/MainActivity.java
@@ -9,6 +9,10 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+/**
+ * Classe d'introduction
+ * modele de la page d'introduction
+ */
 public class MainActivity extends AppCompatActivity {
 
     private Button btnStart;
@@ -16,6 +20,12 @@ public class MainActivity extends AppCompatActivity {
 
     MediaPlayer media1;
 
+    /**
+     * @param savedInstanceState
+     *
+     * Classe utilisée lors de la creation de la page
+     * On y instancie les différents widgets et variables
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,31 +38,58 @@ public class MainActivity extends AppCompatActivity {
         song.execute();
     }
 
+    /**
+     * @param view
+     *
+     * Lance la prochaine page de question
+     */
     public void onClickStart(View view){
         Intent intent = new Intent(this, Page1.class);
         startActivity(intent);
     }
 
+    /**
+     * @param view
+     *
+     * Quitte l'application et stop le son si besoin
+     */
     public void onClickExit(View view) {
         finish();
         media1.stop();
     }
 
+    /**
+     * Classe imbriquée utilisée pour jouer du son
+     */
     class Song extends AsyncTask<Void, Integer, Void> {
 
+        /**
+         * Constructeur vide
+         */
         public void Media() {
         }
 
+        /**
+         * Methode de base pas utilisée
+         */
         @Override
         protected Void doInBackground(Void... voids) { // Se fait en background du thread UI
             return null;
         }
 
+        /**
+         * Methode de base pas utilisée
+         */
         @Override
         protected void onProgressUpdate(Integer... values) {
             super.onProgressUpdate(values);
         }
 
+        /**
+         * @param aVoid
+         *
+         * Methode utilisée pour instancier et jouer le son
+         */
         @Override
         protected void onPostExecute(Void aVoid) { // S'exécute sur le ThreadUI après doInBackground
             super.onPostExecute(aVoid);
